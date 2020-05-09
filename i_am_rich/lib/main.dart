3bcfd83afd56c6e7d8e357a113a7911747a147d8
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-05-09 21:15:20 +0530

    Added an image to the application

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,11 +4,15 @@ void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: new AppBar(
             title: Text("I Am Rich"),
-            backgroundColor: Colors.red,
-            centerTitle: true,
-            elevation: 4,
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+                image: AssetImage('images/diamond.png'),
+            ),
           ),
         ),
       ),
